f06d2fa2546fda2f78b2c137c157e12afb6f3908
Johan809 <20186764@itla.edu.do>
2020-11-24 13:55:11 -0400

    feat (client): regAppoint process

--- client/src/pages/regAppoint/regAppoint.js
+++ client/src/pages/regAppoint/regAppoint.js
@@ -1,5 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { Typography, Checkbox } from '@material-ui/core';
+import React, { useEffect, useState, useContext } from 'react';
+import {
+	Typography,
+	Checkbox,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	Button
+} from '@material-ui/core';
 import {
 	CardTitle,
 	Container,
@@ -12,13 +21,30 @@ import {
 	ImgStyle
 } from './RegAppoint.style';
 import Doctor from 'assets/icons/dr.svg';
+import Form from 'shared/form/Form.styled';
 import Datepicker from 'shared/datepicker/Datepicker';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
+import { MainContext } from 'global/context';
+import axios from 'axios';
 
 export default function RegAppoint() {
 	const [dr, setDr] = useState({});
+	const history = useHistory();
+	const [checked, setChecked] = useState(false);
+	const [openDiag, setOpenDiag] = useState(false);
+	const [date, setDate] = useState(new Date());
+	const { user } = useContext(MainContext).state;
 	const [cost, setCost] = useState(3000);
 	const location = useLocation();
+
+	const handleClickOpen = () => {
+		setOpenDiag(true);
+	};
+
+	const handleClose = () => {
+		setOpenDiag(false);
+		history.push('/');
+	};
 
 	useEffect(() => {
 		document.title = 'Registrar Cita';
@@ -27,6 +53,21 @@ export default function RegAppoint() {
 	useEffect(() => {
 		setDr(location.state);
 	}, [location.state]);
+
+	const handledFormSubmit = async (e) => {
+		e.preventDefault();
+		let newAppoint = {
+			ID_User: user['_id'],
+			ID_Doctor: dr['_id'],
+			ID_Consult: dr['ID_Consult'],
+			emission_date: new Date(),
+			realization_date: date,
+			payment: cost,
+			ins_validation: checked
+		};
+		let { data } = await axios.post('/appoints/createAppoint', newAppoint);
+		if (data['ok']) handleClickOpen();
+	};
 
 	return (
 		<Container>
@@ -42,24 +83,55 @@ export default function RegAppoint() {
 						<br />
 					</Typography>
 
-					<Datepicker
-						id="dateAppoint"
-						label="Fecha de Cita"
-						minDate={new Date()}
-					/>
+					<Form onSubmit={handledFormSubmit}>
+						<Datepicker
+							id="dateAppoint"
+							label="Fecha de Cita"
+							minDate={new Date()}
+							value={date}
+							onChange={(d) => setDate(d)}
+						/>
 
-					<InsuranceCB
-						control={<Checkbox color="primary" name="insuranceCB" />}
-						label="Seguro Médico"
-						onChange={(e) => setCost(!e.target.checked ? 3000 : 950)}
-					/>
+						<InsuranceCB
+							control={<Checkbox color="primary" name="insuranceCB" />}
+							label="Seguro Médico"
+							onChange={(e) => {
+								setCost(!e.target.checked ? 3000 : 950);
+								setChecked(e.target.checked);
+							}}
+						/>
 
-					<CardActionsStyled>
-						<BtnCancel variant="contained">Cancelar</BtnCancel>
-						<BtnAccept variant="contained">Confimar</BtnAccept>
-					</CardActionsStyled>
+						<CardActionsStyled>
+							<BtnCancel variant="contained">Cancelar</BtnCancel>
+							<BtnAccept variant="contained" type="submit">
+								Confimar
+							</BtnAccept>
+						</CardActionsStyled>
+					</Form>
 				</CardBody>
 			</StyledCard>
+			<Dialog
+				open={openDiag}
+				onClose={handleClose}
+				aria-labelledby="alert-dialog-title"
+				aria-describedby="alert-dialog-description"
+			>
+				<DialogTitle id="alert-dialog-title">
+					{'Su Cita ha sido registrada con éxito'}
+				</DialogTitle>
+				<DialogContent>
+					<DialogContentText id="alert-dialog-description">
+						Su petición de cita ha sido enviada al consultorio correspondiente,
+						dentro de unas horas se le dará una respuesta, manténgase atento a
+						su bandeja de citas reservadas.
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose} color="primary" autoFocus>
+						Entendido
+					</Button>
+				</DialogActions>
+			</Dialog>
 		</Container>
 	);
 }
