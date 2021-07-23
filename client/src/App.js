c639a7b05b883470d730bc77a1276353297398c9
cqm2112 <61892716+cqm2112@users.noreply.github.com>
2021-07-23 00:55:40 +0000

    initialize Login and Headboard render

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,15 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 const Home = lazy(() => import('@/pages/Home'));
+const Login = lazy(() => import('@/pages/Login'));
 const About = lazy(() => import('@/pages/About'));
 const Dashboard = lazy(() => import('@/pages/Dashboard'));
 
-function App() {
+function App(props) {
+	var IsVisible = props.IsVisible;
+	IsVisible = true
+	if(IsVisible){
+
 	return (
 		<Router>
 			<Suspense fallback={<div>Loading...</div>}>
@@ -34,6 +39,32 @@ function App() {
 						</Route>
 						<Route path="/dashboard">
 							<Dashboard />
+						</Route>
+						<Route path="/Login">
+							<Login />
+						</Route>
+					</Switch>
+				</div>
+			</Suspense>
+		</Router>
+	);}
+	return(
+		<Router>
+			<Suspense fallback={<div>Loading...</div>}>
+				<div className="App">
+				
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
+						<Route path="/about">
+							<About />
+						</Route>
+						<Route path="/dashboard">
+							<Dashboard />
+						</Route>
+						<Route path="/Login">
+							<Login />
 						</Route>
 					</Switch>
 				</div>
