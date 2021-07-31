bf77d8a7470af958199571a989625343d7ca84fd
Dalvin Molina <dalvingamer@gmail.com>
2021-07-31 17:40:28 -0400

    add icon Menu and create Component Header

--- client/src/App.js
+++ client/src/App.js
@@ -11,50 +11,50 @@ const About = lazy(() => import('@/pages/About'));
 const Dashboard = lazy(() => import('@/pages/Dashboard'));
 
 function App(props) {
-	var IsVisible = props.IsVisible;
-	IsVisible = true;
+	// var IsVisible = props.IsVisible;
+	// IsVisible = true;
 
-	if (IsVisible) {
-		return (
-			<div>
-				<GlobalStyle />
-				<Router>
-					<Suspense fallback={<div>Loading...</div>}>
-						<div className="App">
-							<Header />
-							<ul>
-								<li>
-									<Link to="/Login">Login</Link>
-								</li>
-								<li>
-									<Link to="/Signup">Signup</Link>
-								</li>
-								<li>
-									<Link to="/">Home</Link>
-								</li>
-								<li>
-									<Link to="/about">About</Link>
-								</li>
-								<li>
-									<Link to="/dashboard">Dashboard</Link>
-								</li>
-							</ul>
+	// if (IsVisible) {
+	return (
+		<div>
+			<GlobalStyle />
+			<Router>
+				<Suspense fallback={<div>Loading...</div>}>
+					<div className="App">
+						<Header />
+						<ul>
+							<li>
+								<Link to="/Login">Login</Link>
+							</li>
+							<li>
+								<Link to="/Signup">Signup</Link>
+							</li>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/about">About</Link>
+							</li>
+							<li>
+								<Link to="/dashboard">Dashboard</Link>
+							</li>
+						</ul>
 
-							<hr />
+						<hr />
 
-							<Switch>
-								<Route exact path="/" component={Home} />
-								<Route path="/about" component={About} />
-								<Route path="/dashboard" component={Dashboard} />
-								<Route path="/Login" component={Login} />
-								<Route path="/signup" component={Signup} />
-							</Switch>
-						</div>
-					</Suspense>
-				</Router>
-			</div>
-		);
-	}
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route path="/about" component={About} />
+							<Route path="/dashboard" component={Dashboard} />
+							<Route path="/Login" component={Login} />
+							<Route path="/signup" component={Signup} />
+						</Switch>
+					</div>
+				</Suspense>
+			</Router>
+		</div>
+	);
+	// }
 	// return(
 	// 	<Router>
 	// 		<Suspense fallback={<div>Loading...</div>}>
