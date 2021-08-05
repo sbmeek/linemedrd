84162fcef472b5a23b9df52b3432abd6971e6784
Juand0014 <juand0014@hotmail.com>
2021-08-05 19:47:20 -0400

    configure i18next multilenguage and add configurate in page login

--- client/src/components/formLogin/FormLogin.js
+++ client/src/components/formLogin/FormLogin.js
@@ -1,8 +1,10 @@
 import Link, { ContentLink } from '@/shared/link/Link';
 import Submit from '@/shared/submit/Submit';
 import { Input } from './FormLogin.styles';
+import { useTranslation } from 'react-i18next';
 
 const FormLogin = () => {
+	const { t } = useTranslation();
 	const align = {
 		left: true
 	};
@@ -13,8 +15,8 @@ const FormLogin = () => {
 				<Input
 					type="text"
 					name="name"
-					placeholder="Ingresar correo electrónico o nombre de usuario"
-					aria-label="Ingresar nombre de usuario o correo"
+					placeholder={t('forms.formLogin.inputEmail.placeholder')}
+					aria-label={t('forms.formLogin.inputEmail.areaLabel')}
 					aria-required="true"
 					required
 				/>
@@ -23,17 +25,17 @@ const FormLogin = () => {
 				<Input
 					type="password"
 					name="password"
-					placeholder="Ingresar Contraseña"
-					aria-label="Ingresar nombre de usuario o correo"
+					placeholder={t('forms.formLogin.inputEmail.placeholder')}
+					aria-label={t('forms.formLogin.inputEmail.placeholder')}
 					aria-required="true"
 					required
 				/>
 			</div>
 			<ContentLink {...align}>
-				<Link to="#?">¿Olvidaste la contraseña?</Link>
+				<Link to="#?">{t('forms.formLogin.forgetPassword')}</Link>
 			</ContentLink>
-			<Submit type="submit" aria-label="Iniciar Sesión">
-				Iniciar Sesión
+			<Submit type="submit" aria-label={t('forms.formLogin.startSession')}>
+				{t('forms.formLogin.startSession')}
 			</Submit>
 		</form>
 	);
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './client';
@@ -7,12 +7,16 @@ import { data } from './styles/Theme';
 import App from './App';
 import ThemeState from './context/theme/themeState';
 
+import './i18next';
+
 ReactDOM.render(
 	<React.StrictMode>
 		<ApolloProvider client={client}>
 			<ThemeState>
 				<Main theme={data}>
-					<App />
+					<Suspense fallback={<div>Loading...</div>}>
+						<App />
+					</Suspense>
 				</Main>
 			</ThemeState>
 		</ApolloProvider>
