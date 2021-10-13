cce743915639b988b40001db3c0b78114f411c9f
Juand0014 <juand0014@hotmail.com>
2021-10-13 10:20:01 -0400

    fixed types of validations function

--- client/src/assets/Helpers/utils.ts
+++ client/src/assets/Helpers/utils.ts
@@ -1,17 +1,21 @@
-export const inputEmailValidation = (email: string) => {
+type arrJsonType = {
+	[key: string]: any;
+};
+
+export const inputEmailValidation = (email: string): boolean => {
 	const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 	return !reg.test(email) ? false : true;
 };
 
-export const inputValidation = (input: string) => {
+export const inputValidation = (input: string): boolean => {
 	return input.length === 0 ? false : true;
 };
 
-export const inputNumberValidation = (input: any) => {
+export const inputNumberValidation = (input: string & number): boolean => {
 	return isNaN(input) ? false : inputValidation(input);
 };
 
-export const validateForm = (form: any) => {
+export const validateForm = (form: arrJsonType): boolean => {
 	let isValid = true;
 	for (let key in form) {
 		if (form[key].validation) {
