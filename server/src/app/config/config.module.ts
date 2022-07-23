a0de72481b7200911606bc613b066910615225e7
Johan809 <Johan809@hotmail.com>
2022-07-22 23:00:15 -0400

    refactor(server): config.module

--- server/src/app/config/config.module.ts
+++ server/src/app/config/config.module.ts
@@ -10,71 +10,56 @@ import {
 	serveStaticOptions
 } from 'app/config/options';
 import { ConfigModuleOptions } from 'app/lib/types';
-import { UserModule } from 'app/entities/user/user.module';
-import { UserAdressModule } from 'app/entities/user-adress/user-adress.module';
-import { UserPreferencesModule } from 'app/entities/user-preferences/user-preferences.module';
-import { DaysModule } from 'app/entities/days/days.module';
-import { WorkdayModule } from 'app/entities/workday/workday.module';
-import { PatientModule } from 'app/entities/patient/patient.module';
-import { SpecialtiesModule } from 'app/entities/specialties/specialties.module';
-import { DoctorModule } from 'app/entities/doctor/doctor.module';
-import { ReportModule } from 'app/entities/report/report.module';
-import { AppointmentModule } from 'app/entities/appointment/appointment.module';
-import { AppointmentContentModule } from 'app/entities/appointment-content/appointment-content.module';
-import { AgendaModule } from 'app/entities/agenda/agenda.module';
-import { RecordModule } from 'app/entities/record/record.module';
 import { AuthModule } from 'app/auth/auth.module';
 import { MailModule } from 'app/mail/mail.module';
+import {
+	AgendaModule,
+	AppointmentContentModule,
+	AppointmentModule,
+	DaysModule,
+	DoctorModule,
+	PatientModule,
+	RecordModule,
+	ReportModule,
+	SpecialtiesModule,
+	UserAdressModule,
+	UserModule,
+	UserPreferencesModule,
+	WorkdayModule
+} from 'app/entities';
 
 @Module({})
 export class ConfigModule {
 	static register(options: ConfigModuleOptions): DynamicModule {
-		if (options.env !== 'development')
-			return {
-				module: ConfigModule,
-				imports: [
-					ServeStaticModule.forRoot(serveStaticOptions),
-					MongooseModule.forRoot(dbUri[options.env], mongoOptions),
-					GraphQLModule.forRoot(gqlOptions),
-					UserModule,
-					UserAdressModule,
-					UserPreferencesModule,
-					DaysModule,
-					WorkdayModule,
-					PatientModule,
-					SpecialtiesModule,
-					DoctorModule,
-					ReportModule,
-					AppointmentModule,
-					AppointmentContentModule,
-					AgendaModule,
-					RecordModule,
-					AuthModule,
-					MailModule
-				]
-			};
-		else
-			return {
-				module: ConfigModule,
-				imports: [
-					MongooseModule.forRoot(dbUri[options.env], mongoOptions),
-					GraphQLModule.forRoot(gqlOptions),
-					UserModule,
-					UserAdressModule,
-					UserPreferencesModule,
-					DaysModule,
-					WorkdayModule,
-					PatientModule,
-					SpecialtiesModule,
-					DoctorModule,
-					ReportModule,
-					AppointmentModule,
-					AppointmentContentModule,
-					AgendaModule,
-					RecordModule,
-					AuthModule,
-					MailModule
-				]
-			};
+		const configModule = {
+			module: ConfigModule,
+			imports: [
+				options.env !== 'development'
+					? ServeStaticModule.forRoot(serveStaticOptions)
+					: null,
+				MongooseModule.forRoot(dbUri[options.env], mongoOptions),
+				GraphQLModule.forRoot(gqlOptions),
+				UserModule,
+				UserAdressModule,
+				UserPreferencesModule,
+				DaysModule,
+				WorkdayModule,
+				PatientModule,
+				SpecialtiesModule,
+				DoctorModule,
+				ReportModule,
+				AppointmentModule,
+				AppointmentContentModule,
+				AgendaModule,
+				RecordModule,
+				AuthModule,
+				MailModule
+			]
+		};
+
+		configModule.imports = configModule.imports.filter(module => {
+			return module != null;
+		});
+		return configModule;
 	}
 }
---
+++ server/src/app/entities/index.ts
@@ -0,0 +1,13 @@
+export { UserModule } from './user/user.module';
+export { UserAdressModule } from './user-adress/user-adress.module';
+export { UserPreferencesModule } from './user-preferences/user-preferences.module';
+export { DaysModule } from './days/days.module';
+export { WorkdayModule } from './workday/workday.module';
+export { PatientModule } from './patient/patient.module';
+export { SpecialtiesModule } from './specialties/specialties.module';
+export { DoctorModule } from './doctor/doctor.module';
+export { ReportModule } from './report/report.module';
+export { AppointmentModule } from './appointment/appointment.module';
+export { AppointmentContentModule } from './appointment-content/appointment-content.module';
+export { AgendaModule } from './agenda/agenda.module';
+export { RecordModule } from './record/record.module';
