726370333bd06aa0715e11f81c23cdeb2663c25a
Dalvin Molina <dalvingamer@gmail.com>
2022-07-22 23:43:59 -0400

    style(client): the Link button from navbar move to folder shared.

--- client/src/components/menu/side-top-user/styles.ts
+++ client/src/components/menu/side-top-user/styles.ts
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import { Link as ComponentLink } from 'react-router-dom';
 import { SideTop, ContentTop } from 'shared/side-top';
 
 export const SideUserTop = styled(SideTop)`
@@ -7,21 +6,3 @@ export const SideUserTop = styled(SideTop)`
 `;
 
 export const Content = ContentTop;
-
-const ButtonLink = styled.button`
-	text-decoration: unset;
-	font-size: 1rem;
-	font-family: ${({ theme }) => theme.fonts.segoeui};
-	color: ${({ theme }) => theme.colors.white};
-	margin: 0.6rem 0;
-	max-width: 100%;
-	width: 100%;
-	padding: 0.6rem;
-	background-color: ${({ theme }) => theme.colors.green4};
-	border-radius: 0.25rem;
-	outline: unset;
-	border: none;
-	text-align: center;
-`;
-
-export const Link = ButtonLink.withComponent(ComponentLink);
---
+++ client/src/shared/button-link/index.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+import { Link as ComponentLink } from 'react-router-dom';
+
+const ButtonLink = styled.button`
+	text-decoration: unset;
+	font-size: 1rem;
+	font-family: ${({ theme }) => theme.fonts.segoeui};
+	color: ${({ theme }) => theme.colors.white};
+	margin: 0.6rem 0;
+	max-width: 100%;
+	width: 100%;
+	padding: 0.6rem;
+	background-color: ${({ theme }) => theme.colors.green4};
+	border-radius: 0.25rem;
+	outline: unset;
+	border: none;
+	text-align: center;
+`;
+
+export const Link = ButtonLink.withComponent(ComponentLink);
