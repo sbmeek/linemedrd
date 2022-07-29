501d2ba5512c188290002e15a2e1d7ff4230c823
Dalvin Molina <dalvingamer@gmail.com>
2022-07-29 01:08:27 -0400

    style(client): implement new icon close-icon

---
+++ client/src/assets/icon/close-icon/close-icon.tsx
@@ -0,0 +1,17 @@
+import { SVGProps } from "react"
+
+export const CloseIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg {...props} viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg">
+    <g
+      stroke={props.color ? props.color : "#000"}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={10}
+      strokeWidth={5}
+    >
+      <path d="m17.5 17.5 37 37M54.5 17.5l-37 37" />
+    </g>
+  </svg>
+)
+
+
